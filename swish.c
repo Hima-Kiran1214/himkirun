66dc228f5aaefd5efb77a6943b0e222aef4cb89a
Hima-Kiran1214 <53946742+Hima-Kiran1214@users.noreply.github.com>
2019-08-09 21:18:41 +0530

    Update swish.c

--- swish.c
+++ swish.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int x,p,t=0,s;
+  scanf("%d %d",&x,&p);
+  whlile(x>0)
+  {
+    t=t+x;
+    s=x*(p/100);
+    x=x-s;
+  }
+  printf("%d/n",t);
+  return 0;
+}
 
